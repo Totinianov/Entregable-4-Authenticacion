ffd5fd7c0fe995d05c85a5a7f95c1b5487bfabf9
T.Otiniano <totivas@gmail.com>
2023-06-20 15:19:00 -0500

    1ercomit

---
+++ src/routes/user.router.js
@@ -0,0 +1,45 @@
+const {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update,
+    verifyCode,
+    login,
+    logged,
+    resetPassword,
+    updatePassword,
+} = require("../controllers/user.controlles");
+const express = require("express");
+const verifyJWT = require("../utils/verifyJWT");
+
+const routerUser = express.Router();
+
+routerUser
+    .route("/")
+    .get(getAll) // Leer todos
+    .post(create); // crear
+
+routerUser.route("/login").post(login); // crear login
+
+routerUser
+    .route("/me") // /users/me
+    .get(verifyJWT, logged); //Leer
+
+routerUser
+    .route("/reset_password") // /users/reset_password
+    .post(resetPassword); // crear
+
+routerUser
+    .route("/:id")
+    .get(verifyJWT, getOne) // leer indi
+    .delete(verifyJWT, remove) // borrar
+    .put(verifyJWT, update); // actualizar
+
+routerUser.route("/verify/:code").get(verifyCode); // Leer ver
+
+routerUser
+    .route("/reset_password/:code") // /reset_password/:code
+    .post(updatePassword); // crear
+
+module.exports = routerUser;
